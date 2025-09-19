8d8103ec7abcb6bded4c15f6111aad3c05e140cf
spoilcrystal2d-cpu <spoilcrystal2d@gmail.com>
2025-09-19 23:56:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-CONSUMER_KEY = "JYmzlmdNClfS0k7WCLjr7ylFK"
-CONSUMER_SECRET = "JB31uFy7aNziRbuUuztd8JSZ2m9b6Y1BCVdpGfAccaiQJZ2SdV"
+CONSUMER_KEY = "vgoBBw0i51yHfNkor1FOLdqYO"
+CONSUMER_SECRET = "DXwgTKk4SvDs9IBD1WxCNFiTrIOaqxa80ok9ywFkISQXJIXNXc"
 CALLBACK_URL = "https://twitter-api-j61j.onrender.com/callback"
+
 
 
